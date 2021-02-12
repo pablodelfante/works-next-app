9df5b436266d5dbb69d7e8e1b7428cda65167741
pablo <pablodelfantexp@gmail.com>
2021-02-12 15:26:06 -0300

    agrego variable de entorno HOST_BACKEND en netlify para que cuando este en netlify use esa variable de entorno sino use la de desarrollo localhost

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
 export const COLOR_DARK = 'dark:bg-black';
-export const HOST_BACKEND = 'https://first-strapi-app-backend.herokuapp.com';
+//export const HOST_BACKEND = 'https://first-strapi-app-backend.herokuapp.com';
 //export const HOST_BACKEND = 'http://localhost:1337';
+export const HOST_BACKEND = process.env.HOST_BACKEND || 'http://localhost:1337';
