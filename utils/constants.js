791217b6dfe91213a4bfa1338f8512bca3e56895
pablo <pablodelfantexp@gmail.com>
2021-02-12 14:48:26 -0300

    elimino netlify.toml

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 export const COLOR_DARK = 'dark:bg-black';
 export const HOST_BACKEND = 'https://first-strapi-app-backend.herokuapp.com';
-// export const HOST_BACKEND = 'http://localhost:1337';
+//export const HOST_BACKEND = 'http://localhost:1337';
