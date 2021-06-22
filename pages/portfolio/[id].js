57535c0baf0c23b82513ec2d7ac9f3fda342f4eb
pablo <pablodelfantexp@gmail.com>
2021-06-22 11:25:48 -0300

    add little link on bottom video, in specific work. After update next. All ok

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -66,8 +66,9 @@ export default function Work({ work }) {
                     // este posicionamiento y padding son para mantener la relacion aspecto del video (16/9)
                     <>
                         <div className="my-5" style={{ aspectRatio: "16/9" }}>
-                            <iframe className="w-full h-full" src={url_video} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                            <iframe className="w-full h-full" src={url_video} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                         </div>
+                        <p className="text-sm">Si no ves el video: <a className="underline text-primary" href={url_video} target="_blank">ver</a></p>
                     </>
                 )}
 
