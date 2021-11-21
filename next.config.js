782e3929ae1a09555cefa64a0b95aa8a3ef97374
pablo <pablodelfantexp@gmail.com>
2021-11-21 11:59:58 -0300

    @add comment on next config

--- next.config.js
+++ next.config.js
@@ -18,26 +18,8 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === "development",
     sw: 'sw.js',
     scope: '/',
+    // issuse github
     runtimeCaching,
     buildExcludes: [/middleware-manifest.json$/]
   }
 })
-
-
-// opciones pasadas por un issuse
-// const withPWA = require("next-pwa");
-// const runtimeCaching = require("next-pwa/cache");
-
-// module.exports = withPWA({
-// 	reactStrictMode: true,
-// 	pwa: {
-// 		dest: "public",
-// 		register: true,
-// 		skipWaiting: true,
-// 		runtimeCaching,
-// 		buildExcludes: [/middleware-manifest.json$/]
-// 	}
-// });
-
-// "next": "^12.0.4",
-// "next-pwa": "^5.4.0"
