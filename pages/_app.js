f6fb49e1ae2826ff8c06ea48814466233659f2a3
pablo <pablodelfantexp@gmail.com>
2021-11-23 20:47:01 -0300

    @test enviroment

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,10 @@ import GAnalytics from "components/GAnalytics";
 
 function MyApp({ Component, pageProps }) {
   // check Google analytics
-  if (process.env.NODE_ENV === 'production') {
+  /* if (process.env.NODE_ENV === 'production') {
     console.log('>>>Proyect is on production enviroment')
-  }
+  } */
+  console.log(process.env.NODE_ENV)
   return <>
 
     {/* <GAnalytics/> alone in production enviroment */}
