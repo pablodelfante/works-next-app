b5375fd32ae01fecce8efeb7ce8257da8d3acb7c
pablo <pablodelfantexp@gmail.com>
2021-12-12 16:49:57 -0300

    refactor: remove index from the folders, and rename the name to the index.js file on the directory

--- src/components/portada/index.js
+++ src/components/portada/index.js
@@ -1,1 +1,16 @@
-export {default} from './Portada.js';
+import style from './portada.module.scss';
+
+export default function Portada({ titlePortada, iconoPortada, contentPortada }) {
+
+    return (
+        <div className={style.portada}>
+            <div className='grid grid-flow-col auto-cols-min justify-start items-center gap-5 pb-4'>
+                <h1 className='font-semibold m-0 p-0'>{titlePortada}</h1>
+                {iconoPortada}
+            </div>
+
+
+            <p>{contentPortada}</p>
+        </div>
+    )
+}
