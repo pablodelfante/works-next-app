78c6fa76778dad1c7211631c33290810e56d601d
pablo <pablodelfantexp@gmail.com>
2022-01-29 15:02:03 -0300

    feat: work with component portada

--- src/components/portada/index.js
+++ src/components/portada/index.js
@@ -1,16 +1,15 @@
-import style from './portada.module.scss';
 
 export default function Portada({ titlePortada, iconoPortada, contentPortada }) {
 
+    const stripe = `border-l-8 border-primary pl-5`;
+
     return (
-        <div className={style.portada}>
+        <section className={`${stripe} my-10 md:my-40 md:max-w-sm`}>
             <div className='grid grid-flow-col auto-cols-min justify-start items-center gap-5 pb-4'>
-                <h1 className='font-bold m-0 p-0'>{titlePortada}</h1>
-                {iconoPortada}
+                <h1 className='text-primary font-bold m-0 p-0'>{titlePortada}</h1>
             </div>
 
-
             <p>{contentPortada}</p>
-        </div>
+        </section>
     )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
 export const COLOR_DARK = 'dark:bg-black';
 export const HOST_BACKEND = process.env.HOST_BACKEND || 'http://localhost:1337';
-//process.env.PURGUE_CSS || false; esto esta en talwind.config
