471e0c309fe13ca9d9e49e6e1f16e0a3c2ca5ce8
pablo <pablodelfantexp@gmail.com>
2022-01-30 15:40:15 -0300

    chore: work on title page

--- src/components/titlePage/index.js
+++ src/components/titlePage/index.js
@@ -7,10 +7,10 @@ export default function TitlePage({ titlePortada, contentPortada }) {
     const stripe = `border-l-8 border-primary pl-5`;
 
     return (
-        <section className="my-10 md:my-40 grid grid-cols-[2fr_1fr] items-center">
+        <section className="my-10 md:my-30 grid grid-cols-[1fr_1fr] items-center">
             <section className={`${stripe} md:max-w-sm h-min`}>
                 <h1 className='text-primary dark:text-primary font-bold mb-4 w-min'>{titlePortada}</h1>
-                <p>{contentPortada}</p>
+                <p className="text-xl font-light">{contentPortada}</p>
             </section>
             <aside>
                 {/* <Blob /> */}
