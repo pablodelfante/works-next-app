ae8261d8f7794ec86b944ab35fca932e85271070
pablo <pablodelfantexp@gmail.com>
2022-01-30 21:08:12 -0300

    feat: work in title top page

--- src/components/titlePage/index.js
+++ src/components/titlePage/index.js
@@ -1,20 +1,18 @@
 import Blob from "components/blob";
-import BlobV2 from "components/blob/blobV2";
 
 
 export default function TitlePage({ titlePortada, contentPortada }) {
 
-    const stripe = `border-l-8 border-primary pl-5`;
+    const stripe = `border-l-[0.8rem] border-primary pl-5`;
 
     return (
-        <section className="my-10 md:my-30 grid grid-cols-[1fr_1fr] items-center">
+        <section className="my-10 md:my-30 grid grid-cols-[1.5fr_1fr] items-center">
             <section className={`${stripe} md:max-w-sm h-min`}>
                 <h1 className='text-primary dark:text-primary font-bold mb-4 w-min'>{titlePortada}</h1>
                 <p className="text-xl font-light">{contentPortada}</p>
             </section>
             <aside>
-                {/* <Blob /> */}
-                <BlobV2/>
+                <Blob/>
             </aside>
         </section>
     )
