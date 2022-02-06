1bd359a620f709cfe640274fedbba9d03148de74
pablo <pablodelfantexp@gmail.com>
2022-02-06 13:57:06 -0300

    fix: set new enviroment variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const COLOR_DARK = 'dark:bg-black';
-export const HOST_BACKEND =  'http://localhost:1337' || process.env.HOST_BACKEND;
+export const HOST_BACKEND =  process.env.HOST_BACKEND_LOCAL || process.env.HOST_BACKEND;
