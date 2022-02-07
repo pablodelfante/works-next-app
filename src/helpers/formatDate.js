1b96961847ab63a1cffca33c77c687b6a2695573
pablo <pablodelfantexp@gmail.com>
2022-02-06 22:52:01 -0300

    chore: translate function to transform date to local string

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -2,7 +2,7 @@ const formatDate = (date) => {
     var published_at = new Date(`${date}`);
     var options = { year: "numeric", month: "long", day: "numeric" };
     
-    return published_at.toLocaleDateString("es-ES", options);
+    return published_at.toLocaleDateString("en-us", options);
 }
 
 export default formatDate;
