0072a75b16e9b9f6d7869c7259473d9d283eeea9
pabla <pablodelfante@vexels..com>
2022-06-05 18:38:58 -0300

    chore: add dotenv like dependencie for vitest

--- test/checkBackendStatus.test.js
+++ test/checkBackendStatus.test.js
@@ -1,9 +1,9 @@
 import { assert, expect, test } from 'vitest'
-import { getWorks } from 'connectors/findWorks';
-import { HOST_BACKEND } from "utils/constants"; 
+import fetch from 'node-fetch';
+import 'dotenv/config'
 
 test('checking backend is running', async () => {
-  // console.log(fetch);
+  console.log(process.env.HOST_BACKEND)
   expect(true).toBe(true);
 
 })
