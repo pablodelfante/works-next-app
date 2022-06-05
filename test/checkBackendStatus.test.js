b9ce865ab6443d6195e938ff140953141060a4f1
pabla <pablodelfante@vexels..com>
2022-06-05 18:07:11 -0300

    chore: add resolve path plugin for viteTest tool

--- test/checkBackendStatus.test.js
+++ test/checkBackendStatus.test.js
@@ -1,6 +1,9 @@
 import { assert, expect, test } from 'vitest'
+import { getWorks } from 'connectors/findWorks';
+import { HOST_BACKEND } from "utils/constants"; 
 
-test('checking backend is running', () => {
+test('checking backend is running', async () => {
+  // console.log(fetch);
   expect(true).toBe(true);
 
 })
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from "path";
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
-    // ...
+  //  root: "src/",
+  //  includeSource: ['src/**/*.{js,ts}'],
   },
+ /*  resolve: {
+    alias: {
+      "$": resolve(__dirname, "src"),
+    },
+  }, */
 })
