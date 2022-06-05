7fbdd9badf8dfff309077ea6a259677cee8b43e2
pabla <pablodelfante@vexels..com>
2022-06-05 18:50:10 -0300

    chore: add base query graphql

--- test/checkBackendStatus.test.js
+++ test/checkBackendStatus.test.js
@@ -3,7 +3,28 @@ import fetch from 'node-fetch';
 import 'dotenv/config'
 
 test('checking backend is running', async () => {
-  console.log(process.env.HOST_BACKEND)
+  const response = await fetch(process.env.HOST_BACKEND, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify({
+        query: `
+        {
+          works {
+            data {
+              id
+            }
+          }
+        }
+        
+        
+        `
+    }),
+})
+  console.log('status response: ', response.status)
+  console.log(await response.json())
   expect(true).toBe(true);
 
 })
