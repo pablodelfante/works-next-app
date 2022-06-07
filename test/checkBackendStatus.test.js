4fea67dea319b7aecc0d8b35fc558d46b7c8c491
pabla <pablodelfante@vexels..com>
2022-06-06 21:12:36 -0300

    test: refactor test

--- test/checkBackendStatus.test.js
+++ test/checkBackendStatus.test.js
@@ -1,16 +1,16 @@
-import { assert, expect, test } from 'vitest'
+import { assert, describe, expect, test } from 'vitest';
 import fetch from 'node-fetch';
-import 'dotenv/config'
+import 'dotenv/config';
 
-test('checking backend is running', async () => {
-  const response = await fetch(process.env.HOST_BACKEND, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-    },
-    body: JSON.stringify({
-        query: `
+describe('checking if backend is running', async () => {
+	const response = await fetch(process.env.HOST_BACKEND, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json'
+		},
+		body: JSON.stringify({
+			query: `
         {
           works {
             data {
@@ -18,13 +18,19 @@ test('checking backend is running', async () => {
             }
           }
         }
-        
-        
         `
-    }),
-})
-  console.log('status response: ', response.status)
-  console.log(await response.json())
-  expect(true).toBe(true);
+		})
+	});
 
-})
+	test('checking server status', async () => {
+		expect(response.status).toEqual(200);
+	});
+	test('checking data on response', async () => {
+		const jsonResponse = await response.json();
+		const { data: { works: { data: works } } } = jsonResponse;
+		expect(works.length).toBeGreaterThan(0);
+	});
+	test('fake test', () => {
+		expect(3).toEqual(3);
+	});
+});
