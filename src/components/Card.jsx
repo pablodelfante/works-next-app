ed7bfb143ad5b402d61c165e6ca005e9d52d3616
pabla <pablodelfante@vexels..com>
2022-08-29 22:32:40 -0300

    chore: fix card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-export default function Card({ work: {attributes: work}, index }) {
+export default function Card({ work, index }) {
     return (
         <>
             <li className="py-3 md:py-8 rounded border hover:border-primary hover:shadow-lg hover:shadow-indigo-500/40 transition duration-300">
