df492068a980aa5f1a1eb8a74b6f893b59837a3d
pabla <pablodelfantexp@gmail.com>
2022-11-27 15:36:49 -0300

    refactoring card remove package

--- src/components/template/index.js
+++ src/components/template/index.js
@@ -1,43 +1,35 @@
-import Header from "./Header";
+import Header from './Header'
 import Footer from './Footer'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import NProgress from 'nprogress';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
 
-
-
-
-export default function Layout({children}) {
-    const router = useRouter();
+export default function Layout({ children }) {
+    const router = useRouter()
 
     useEffect(() => {
         const handler = (url) => {
-            NProgress.start();
+            NProgress.start()
         }
 
-        router.events.on('routeChangeStart', handler);
-        router.events.on('routeChangeComplete', (url) => NProgress.done());
+        router.events.on('routeChangeStart', handler)
+        router.events.on('routeChangeComplete', (url) => NProgress.done())
 
         return () => {
-            router.events.off('routeChangeStart', handler);
+            router.events.off('routeChangeStart', handler)
         }
     }, [])
 
     return (
-        <div
-            className='container m-auto overflow-hidden'>
+        <div className="container m-auto">
             {/* HEADER */}
             <Header />
 
             {/* CONTENIDO PRINCIPAL */}
-            <main className=''>
-                {children}
-            </main>
+            <main className="">{children}</main>
 
             {/* FOOTER */}
             <Footer />
-
-
         </div>
     )
 }
