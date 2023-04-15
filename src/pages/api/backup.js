e6dabc5d7616ed746795eb6eeab03b118f7bd3e1
pabla <pablodelfantexp@gmail.com>
2023-04-14 21:19:14 -0300

    simple data backups

---
+++ src/pages/api/backup.js
@@ -0,0 +1,10 @@
+import { getWorks } from 'connectors/findWorks'
+export default async function handler(req, res) {
+    try {
+        const works = await getWorks()
+        res.status(200).json(works)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ message: 'error on try to get works' })
+    }
+}
