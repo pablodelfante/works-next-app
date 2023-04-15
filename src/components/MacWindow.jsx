902b015a29ed31163b80e2afa58e61dede12f930
pabla <pablodelfantexp@gmail.com>
2023-04-15 20:12:32 -0300

    fix

--- src/components/MacWindow.jsx
+++ src/components/MacWindow.jsx
@@ -1,6 +1,6 @@
 function MacWindow({ children }) {
     return (
-        <div className="bg-slate-500 dark:bg-gray-50 rounded-lg shadow-lg">
+        <div className="bg-slate-500 dark:bg-gray-50 rounded-lg shadow-lg overflow-clip">
             <div className="flex gap-1 p-2">
                 <div className="rounded-full bg-red-500 w-3 h-3" />
                 <div className="rounded-full bg-yellow-400 w-3 h-3" />
