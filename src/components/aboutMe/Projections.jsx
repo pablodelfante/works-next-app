de553c711f78331bb42ab6d963326ae5cdbd9055
pabla <pablodelfante@vexels..com>
2023-04-20 21:35:03 -0300

    fix terxt white last terminal

--- src/components/aboutMe/Projections.jsx
+++ src/components/aboutMe/Projections.jsx
@@ -6,14 +6,14 @@ export default function WhoIam() {
     return (
         <div className="grid justify-center gap-8">
             <h3>Projections</h3>
-            <div className="lg:w-[40em] text-white">
+            <div className="lg:w-[40em] ">
                 <Terminal init="projections">
-                    <p className="mb-8">
+                    <p className="mb-8 text-white">
                         I am interested in a stacks like JAM, technologies like
                         PWA, serverless products, AIs and others. This does not
                         mean that I don't consider other forms.
                     </p>
-                    <p>
+                    <p className="text-white">
                         In my opinion these tools can help improve the user
                         experinece to create systems to enhance people lives. I
                         am excited about accepting challenges, improving my
