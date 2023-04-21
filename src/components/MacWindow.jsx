f536706040c35b561c9e6e175cc18d4bff3e8984
pabla <pablodelfantexp@gmail.com>
2023-04-21 20:28:34 -0300

    fix border terminal

--- src/components/MacWindow.jsx
+++ src/components/MacWindow.jsx
@@ -1,7 +1,7 @@
 function MacWindow({ children }) {
     return (
-        <div className="bg-slate-500 dark:bg-gray-50 rounded-lg shadow-lg overflow-clip">
-            <div className="flex gap-1 p-2">
+        <div className="rounded-lg shadow-lg overflow-clip">
+            <div className="flex gap-1 p-2 bg-slate-500 dark:bg-gray-50">
                 <div className="rounded-full bg-red-500 w-3 h-3" />
                 <div className="rounded-full bg-yellow-400 w-3 h-3" />
                 <div className="rounded-full bg-green-500 w-3 h-3" />
