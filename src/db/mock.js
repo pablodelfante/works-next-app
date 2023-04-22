e60a8bf8d1c59ed29d693c62629c13bb735f4864
pabla <pablodelfante@vexels..com>
2023-04-22 15:03:45 -0300

    before translate data

---
+++ src/db/mock.js
@@ -0,0 +1,128 @@
+export const mock = [
+    {
+        id: 'clgec3grj45gi0bljvhbrltli',
+        title: 'Hemocentro',
+        description:
+            'The Hemocentro project is part and objective of the degree completion project, which aims, among other things, to show a little of the evolution of the profile acquired during the course of the degree. Exhibit and work in different areas such as management, marketing, development and design.',
+        stage: 'DRAFT',
+        highlighted: true,
+        githubUrl: 'https://github.com/pablo-university/web-project',
+        image: { url: 'https://media.graphassets.com/7oiTVlNWQdDkHhW3jbNA' },
+        tags: ['nextJs', 'figma', 'docker', 'hasura', 'postgress'],
+        components: [],
+    },
+    {
+        id: 'clgebpniz458o0ale7dygmodk',
+        title: 'PWA - This web site',
+        description:
+            'I try to show my profile both as a professional and as an individual. Every day I try to get better, to learn, explore and in this way be a better person enjoying what I do. I build this website with a Jamstack and used next and hygraph as my main technologies stack.',
+        stage: 'DRAFT',
+        highlighted: true,
+        githubUrl: 'https://github.com/pablodelfante/works-next-app',
+        image: { url: 'https://media.graphassets.com/cd0keiuCRHat2OfC6haG' },
+        tags: ['nextJs', 'pwa', 'docker', 'tailwind', 'hygraph', 'graphql', 'figma'],
+        components: [],
+    },
+    {
+        id: 'clgec4ep146rg0bl9e2r9h5kt',
+        title: 'Web workers',
+        description:
+            'I have recently been learning about Web Workers, I had a curiosity about functionality and can have deep knowledge about this. I have Always been curious about javascript, technical details and for example, the event loop, have paved the way for me to get more knowledge about technical improvement, and performance and this knowledge has led me to search and investigate other tags, such as web APIs, PWA’s and others. I have thought of a process that requires some more powerful for example, image management and others. My first point to start with was not blocking the main thread on js. This was my point for learning and investigating other alternative to do some tasks that required more power in the background.',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: 'https://github.com/pablo-learn/workers',
+        image: { url: 'https://media.graphassets.com/H6Yw8BCvR9idWeCpPikQ' },
+        tags: ['node', 'expressJs', 'webWorkers', 'ssr'],
+        components: [],
+    },
+    {
+        id: 'clgec20v345fg0bljs6thxcmj',
+        title: 'Prototype - NATURAL',
+        description:
+            'Academic project, the target was to understand matters and different proceses in diciplines such as UX and UI, had and introduction to the digital interfaces world web develop.',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: null,
+        image: { url: 'https://media.graphassets.com/KLdmTcqMSdCFp5hukJKc' },
+        tags: ['figma', 'photoshoop', 'ilustrator'],
+        components: [{ __typename: 'Video', videoUrl: 'https://www.youtube.com/embed/3cLymG7PwM4' }],
+    },
+    {
+        id: 'clgec0plc46p00bl9f7c98fz6',
+        title: 'DW2 - Strip to Australia',
+        description:
+            'Overall, it is about tourist information, general information about Australia, the target of the web is to show differents skills thgrough with techologies as CSS, SCSS or the different libraries implementation to do the web site.',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: 'https://github.com/pablodelfante/W2-obligatorio2',
+        image: { url: 'https://media.graphassets.com/yMSoiraR5C6CoTjqSz3D' },
+        tags: ['html', 'css', 'scss', 'javascript'],
+        components: [],
+    },
+    {
+        id: 'clgebwo1f45dw0aletm1m9ius',
+        title: 'PWA - Garden app',
+        description:
+            'This app is a draft. I would like to show this from an early stage. The objective is to understand the concept of pwa. Practising the written code and implementing. Different new web tags , with Svelte framework firebase and more.',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: 'https://github.com/pablodelfante/svelte-proyects/tree/pwa-garden',
+        image: { url: 'https://media.graphassets.com/HY6PL2isS0ueD57DE04O' },
+        tags: ['svelte', 'pwa', 'firebase', 'bootstrap'],
+        components: [],
+    },
+    {
+        id: 'clgebrif446js0bl9txdgq78s',
+        title: 'Socket.io - Express Chat App',
+        description:
+            'Here the socket io implementation on chat application is shown as a recently acquired technology. I use cookies management and a basic system of sessions to register the current user.',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: 'https://github.com/pablodelfante/socket-chat-app',
+        image: { url: 'https://media.graphassets.com/KenWjne7QiOrLClZutNF' },
+        tags: ['expressJs', 'tailwind', 'socketIo', 'nodemon'],
+        components: [],
+    },
+    {
+        id: 'clgebm3qm455p0bljq5mrklpz',
+        title: 'P2 - Watch Shop',
+        description:
+            'Academic project, use PHP and MYSQL as database, the proposal was do two sections in the target. One where you had the client and another, with a dashboard panel for content managment. I underlineed the personal growth out of the proyect and the evolution on structures of data and organization, work methodologies and experience obtained with front and back tools. This opened experiences and doors to other projects with node and different databases.',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: 'https://github.com/pablodelfante/P2-obligatorio',
+        image: { url: 'https://media.graphassets.com/MGNOst36Q2ypnxM4AKZh' },
+        tags: ['php', 'mySQL', 'browserSync'],
+        components: [{ __typename: 'Video', videoUrl: 'https://www.youtube.com/embed/2Uspqad9kaU' }],
+    },
+    {
+        id: 'clgeb4wza45080ale9pjc0qy7',
+        title: 'DW3 - Neuralink Redesign',
+        description:
+            'Redesign of the web Neuralink, the objective of the work was to get a website and doing the redesign. I focused on UX and project structure, made a performance improvement in comparison with the original website, overall produce fast and more fluid navegation.',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: 'https://github.com/pablodelfante/W3-Obligatorio1',
+        image: { url: 'https://media.graphassets.com/Euua1p04SIac3egQANVJ' },
+        tags: ['html', 'css', 'scss', 'javascript', 'gulp'],
+        components: [{ __typename: 'Video', videoUrl: 'https://www.youtube.com/embed/zwcDITgrw-0' }],
+    },
+    {
+        id: 'clgdn9vpu3ouo0bk6qopdowfg',
+        title: 'JS - Dashboard people managment',
+        description:
+            'Dashboard for people managment, created with Javascript vanilla, the focus was to show the skills obtained with javascript and jquery like library to understand the libraries concept. And other integratios!',
+        stage: 'DRAFT',
+        highlighted: false,
+        githubUrl: 'https://github.com/pablodelfante/P1-obligatorio',
+        image: { url: 'https://media.graphassets.com/YcLcos57Tr2sy2w9Z9OY' },
+        tags: ['html', 'javascript', 'figma', 'scss', 'node'],
+        components: [
+            {
+                __typename: 'Markdown',
+                markdown:
+                    '### Node integration\nThe system, manage the users getting the data from localstorage or local with ajax, save the data on localstorage and after that try use the server express who manage the local saved.',
+            },
+        ],
+    },
+]
