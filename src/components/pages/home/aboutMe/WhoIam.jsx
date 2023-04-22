9e12c2e3895f8faa752f63f591a69d7c8056d9cb
pabla <pablodelfante@vexels..com>
2023-04-22 01:46:14 -0300

    mini refactor who i am

--- src/components/pages/home/aboutMe/WhoIam.jsx
+++ src/components/pages/home/aboutMe/WhoIam.jsx
@@ -11,12 +11,7 @@ export default function WhoIam() {
     return (
         <div className="grid justify-center grid-flow-row md:grid-flow-col gap-16">
             <div>
-                <img
-                    className="max-w-sm"
-                    src={maskPng.src}
-                    alt=""
-                    ref={maskedElement}
-                />
+                <img className="max-w-sm" src={maskPng.src} alt="" ref={maskedElement} />
                 {/*
                  TODO: to solve
                  <BlobV2
@@ -28,19 +23,14 @@ export default function WhoIam() {
                     /> */}
             </div>
             <div>
-                <div className="lg:w-[35em]">
+                <div className="grid gap-6 lg:w-[35em]">
+                    <h3>Who am I?</h3>
                     <Terminal init="whoIam">
                         <p className="mb-5 text-white">
-                            I am a graphic designer and I am currrently studying
-                            web design. I have been passionate about technology
-                            since I was a curious and active child. I love
-                            sharing, learning and helping others as well as
-                            meeting new people!
+                            I am a graphic designer and I am currrently studying web design. I have been passionate about technology since I was a
+                            curious and active child. I love sharing, learning and helping others as well as meeting new people!
                         </p>
-                        <p className="text-white">
-                            I love sorting out problems and I really enjoy being
-                            analytic and creative!
-                        </p>
+                        <p className="text-white">I love sorting out problems and I really enjoy being analytic and creative!</p>
                     </Terminal>
                 </div>
             </div>
