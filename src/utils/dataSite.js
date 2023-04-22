28bd93f839374f2abb7f4ae8d41fa47b1bca231d
pabla <pablodelfante@vexels..com>
2023-04-22 14:34:03 -0300

    update title and remove unused components on portfolio page

--- src/utils/dataSite.js
+++
@@ -1,7 +0,0 @@
-// Contenidos de portadas (actualizo desde aca)
-export const CONTENT_PORTADA = {
-    portfolio: {
-        titlePortada: `Nice to see you!!`,
-        contentPortada: `Hi!! welcome to my site! Here I'm going to tell you about me, about myself. Go ahead!`,
-    },
-}
