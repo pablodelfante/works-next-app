024c787038696ec13dc4c9a30dfa3aa4977b2378
pabla <pablodelfante@vexels..com>
2023-04-22 01:44:21 -0300

    restore works get

--- src/pages/index.js
+++ src/pages/index.js
@@ -96,7 +96,7 @@ export default function Home({ works }) {
 
 // get all the posts
 export async function getStaticProps() {
-    // const works = await getWorks()
-    const works = []
+    const works = await getWorks()
+    //const works = []
     return { props: { works } }
 }
