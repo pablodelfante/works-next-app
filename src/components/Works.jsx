575f2a637c170cfa4f8abaa192232b72ff0a503b
pabla <pablodelfantexp@gmail.com>
2023-05-25 20:54:22 -0300

    remove unused code

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,9 +2,6 @@ import Link from 'next/link'
 import Card from 'components/Card'
 
 function Works({ works = null }) {
-    const draftControl = (draft) => process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production' || draft !== 'DRAFT'
-
-    const worksAvailablesToRender = works.filter((work) => true)
     return (
         <>
             <ul className="grid grid-cols-1 xl:grid-cols-2 gap-10 items-stretch">
@@ -16,7 +13,7 @@ function Works({ works = null }) {
                     </p>
                 )}
 
-                {worksAvailablesToRender?.map(({ id, image: { url: imageUrl }, title, description, tags }) => (
+                {works?.map(({ id, image: { url: imageUrl }, title, description, tags }) => (
                     <Link href={`/portfolio/${id}`} key={id}>
                         {/* rome-ignore lint/a11y/useValidAnchor: <explanation> */}
                         <div className="grid">
