292a70efc9aa66a2f7cbd84f3241674794e4e80a
pabla <pablodelfantexp@gmail.com>
2023-08-12 20:47:22 -0300

    rm pges

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -15,23 +15,21 @@ export default async function Portfolio() {
             </Head>
             <Layout>
                 <Container>
-                    <section>
-                        <section className="my-10 md:my-30 grid lg:grid-cols-[1.5fr_1fr] items-center">
-                            <div className={`md:max-w-sm h-min`}>
-                                <h1 className="text-primary dark:text-primary font-bold mb-4 lg:w-min text-6xl lg:text-8xl">Nice to see you!!</h1>
-                                <Terminal init="portfolio">
-                                    Hi!! welcome to my site! Here I'm going to tell you about me, about myself. Go ahead!
-                                </Terminal>
-                            </div>
-                            <div className="hidden lg:block">
-                                <BlobV2
-                                    blobConfig={{
-                                        color: 'hwb(243deg 30% 1%)',
-                                    }}
-                                    canvasStyles={{ width: '400px' }}
-                                />
-                            </div>
-                        </section>
+                    <section className="my-10 md:my-30 grid lg:grid-cols-[1.5fr_1fr] items-center">
+                        <div className="grid gap-5 md:max-w-sm h-min">
+                            <h1 className="text-primary dark:text-primary font-bold lg:w-min text-6xl lg:text-8xl">Nice to see you!!</h1>
+                            <Terminal init="portfolio">
+                                Hi!! welcome to my site! Here I'm going to tell you about me, about myself. Go ahead!
+                            </Terminal>
+                        </div>
+                        <div className="hidden lg:block">
+                            <BlobV2
+                                blobConfig={{
+                                    color: 'hwb(243deg 30% 1%)',
+                                }}
+                                canvasStyles={{ width: '400px' }}
+                            />
+                        </div>
                     </section>
                     <section className="my-20 lg:my-40">
                         <Works works={works} />
