b4770cc88449a4bc3623ba112b33b25b1ffdf5b9
pabla <pablodelfantexp@gmail.com>
2024-06-22 13:15:55 -0300

    feat: Add lazy loading for main image on experience page

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -1,7 +1,23 @@
 import Layout from 'components/template'
+import Container from 'components/layouts/Container'
+import main from 'app/experience/main.png'
 
 const Xp = () => {
-    return <Layout>xp page</Layout>
+    return (
+        <Layout>
+            <Container>
+                <div className="grid justify-center">
+                    <img width={300} height={200} className="col-start-1 grid- col-end-2 row-start-1 row-end-2 w-64" src={main.src} alt="" />
+                    <div className="col-start-1 col-end-2 row-start-1 row-end-2 pl-8">
+                        <h1 className="text-center grid">
+                            <span className="pl-32 text-9xl font-bold">My</span>
+                            <span className="text-7xl font-bold">Journey</span>
+                        </h1>
+                    </div>
+                </div>
+            </Container>
+        </Layout>
+    )
 }
 
 export default Xp
