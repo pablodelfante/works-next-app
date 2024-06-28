88bc614b6905b630429a50f892ce931aadacf9c6
pabla <pablodelfantexp@gmail.com>
2024-06-28 14:54:28 -0300

    Refactor Pill component to use dark text color for outline type
    Update the Pill component to use the dark text color for the outline type. This change improves the visibility and ensures consistency with the design guidelines.

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -1,7 +1,7 @@
 const Pill = ({ children, type }) => {
     if (type === 'outline') {
         return (
-            <div className="text-white text-xs font-medium truncate px-2 py-1 border border-primary rounded-full hover:bg-primary transition select-none">
+            <div className="text-primary dark:text-white text-xs font-medium truncate px-2 py-1 border border-primary rounded-full hover:bg-primary transition select-none">
                 {children}
             </div>
         )
