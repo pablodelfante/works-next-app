797f3a693ed162c3309ee336296476c513a2f916
pabla <pablodelfantexp@gmail.com>
2024-08-20 18:06:13 -0300

    improve code readability

--- src/components/XpItem.jsx
+++ src/components/XpItem.jsx
@@ -2,17 +2,17 @@
 import { useState } from 'react'
 
 const ItemXp = ({ title, children, disableShowMore = false, icon }) => {
-    const [state, setState] = useState(false)
+    const [showMore, setShowMore] = useState(false)
     return (
         <div className="grid gap-2 fade-in-from-right">
             <h4 className="flex gap-2 text-lg">
                 {icon}
                 {title}
             </h4>
-            {children(state)}
+            {children(showMore)}
             {!disableShowMore && (
-                <span className="font-light cursor-pointer underline" onClick={() => setState(!state)}>
-                    {state ? 'show less' : 'show more'}
+                <span className="font-light cursor-pointer underline" onClick={() => setShowMore(!showMore)}>
+                    {showMore ? 'show less' : 'show more'}
                 </span>
             )}
         </div>
