798bf9fbea226e4415b9ba79b2b0bb15b7147090
pabla <pablodelfantexp@gmail.com>
2025-04-01 08:38:23 -0300

    Revert "Implement TypeScript support for findWorks: replace JavaScript file with TypeScript version, add type definitions for GraphQL response and components, and update tsconfig.json to include new file and adjust baseUrl."
    This reverts commit 462d4a2e5994c46396a342b82598a13fc280d9c7.

---
+++ src/connectors/findWorks.js
@@ -0,0 +1,64 @@
+import { mock } from 'db/mock'
+import { IS_BACKEND_MOCKED } from 'utils/constants'
+
+async function fetchWorks() {
+  const query = `
+      query Works {
+        works(orderBy: highlighted_DESC, first: 20, stage: PUBLISHED) {
+          id
+          title
+          description
+          stage
+          highlighted
+          githubUrl
+          deployUrl
+          image {
+            url
+          }
+          tags
+          components {
+            __typename
+            ... on Video {
+              videoUrl
+            }
+            ... on Markdown {
+              markdown
+            }
+            ... on Image {
+              image {
+                url
+              }
+            }
+          }
+        }
+      }
+    `
+
+  try {
+    const response = await fetch(process.env.HOST_BACKEND, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    })
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+    throw new Error(error)
+  }
+}
+export const fetchWorksMock = () => mock
+export const getWorks = async () => {
+  try {
+    if (IS_BACKEND_MOCKED) return fetchWorksMock()
+    const response = await fetchWorks()
+    const works = response.data.works
+    return works
+  } catch (error) {
+    console.error({ getWorksIds: error })
+    throw new Error(error)
+  }
+}
