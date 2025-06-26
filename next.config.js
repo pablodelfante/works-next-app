4a7249a8c826a50620930aff24370d2e4d06f471
pabla <pablodelfantexp@gmail.com>
2025-06-26 18:07:50 -0300

    fix images

--- next.config.js
+++ next.config.js
@@ -3,14 +3,8 @@ const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
     images: {
-        domains: ['res.cloudinary.com', 'blog.aulaformativa.com', 'media.giphy.com', 'avatars.githubusercontent.com', 'media.graphassets.com'],
+        domains: ['res.cloudinary.com', 'blog.aulaformativa.com', 'media.giphy.com', 'avatars.githubusercontent.com', 'media.graphassets.com', 'us-east-1-shared-usea1-02.graphassets.com'],
     },
-    remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: '**.graphassets.com',
-        },
-    ],
     // https://dev.to/byteslash/how-to-create-a-pwa-with-next-js-4dbm
     pwa: {
         dest: 'public',
